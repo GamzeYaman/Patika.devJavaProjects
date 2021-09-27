a3633252762692fb75840ea6502d2cc1ce1bc385
Mikrokosmos86 <Mikrokosmos.86@gmail.com>
2021-09-27 22:20:50 +0300

    Finding perfect number

---
+++ Java101_PracticesAndProjects/PerfectNumber/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter a number : ");
+		  int number = scan.nextInt();
+		 
+		int sum = 0;
+		for(int i = 1; i < number; i++) {
+			if(number % i == 0) {
+				sum += i;
+			}
+		}
+		
+		if(sum == number) {
+			System.out.println(number + " is a perfect number.");
+		}else {
+			System.out.println(number + " isn't a perfect number.");
+		}
+	}
+
+}
